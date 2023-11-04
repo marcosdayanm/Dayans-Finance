95f8bf13ccc8910baba9d81f0a4da88d72b9a1f6
Marcos Dayan Mann <75103719+marcosdayanm@users.noreply.github.com>
2023-11-03 22:45:05 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -317,9 +317,7 @@ def quote():
 
 
 
-
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
-
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True, use_reloader=True)
 
